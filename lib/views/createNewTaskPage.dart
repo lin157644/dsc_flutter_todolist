12d288dfa7383297d1e9b91d859084aed9890210
FlutLab.io <>
2021-04-10 06:19:53 +0000

    Autosave

--- lib/views/createNewTaskPage.dart
+++ lib/views/createNewTaskPage.dart
@@ -240,8 +240,7 @@ class _CreateNewTaskPageState extends State<CreateNewTaskPage> {
               ),
             ),
             // Create Task
-            //Container
-            Container()(
+            Container(
               width: double.infinity,
               //FlatButton 在Flutter2.0棄用 請改用TextButton
               child: FlatButton(
