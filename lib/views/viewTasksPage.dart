7216df7c72817b6a9d6fedcee087e8cb1c6d6875
FlutLab.io <>
2021-04-10 06:42:55 +0000

    Autosave

--- lib/views/viewTasksPage.dart
+++ lib/views/viewTasksPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'createNewTaskPage.dart';
+import '../models/taskList.dart';
 
 class ViewTasksPage extends StatefulWidget {
   ViewTasksPage({Key key}) : super(key: key);
@@ -9,6 +10,7 @@ class ViewTasksPage extends StatefulWidget {
 }
 
 class _ViewTasksPageState extends State<ViewTasksPage> {
+  TaskList _taskList = TaskList();
   @override
   void initState() {
     super.initState();
@@ -27,6 +29,11 @@ class _ViewTasksPageState extends State<ViewTasksPage> {
         padding: const EdgeInsets.symmetric(horizontal: 25),
         child: Column(
           children: [
+            // Fixed Spacing
+            SizedBox(
+              height: 78,
+            ),
+
             // Title
             Container(
               alignment: Alignment.centerLeft,
@@ -41,6 +48,127 @@ class _ViewTasksPageState extends State<ViewTasksPage> {
                 ),
               ),
             ),
+
+            // Fixed Spacing
+            SizedBox(
+              height: 48,
+            ),
+
+            // Task List
+            Expanded(
+              flex: 9,
+              child: ListView.separated(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {},
+                    onLongPress: () {},
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 18,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: _taskList.task[index].isPinned
+                            ? Border.all(
+                                color: Colors.blueGrey[900],
+                                width: 2.0,
+                              )
+                            : Border.all(color: Colors.blueGrey[100]),
+                        color: _taskList.task[index].isDone ? const Color(0xFFFFBC0A) : Colors.transparent,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Todo String
+                              Expanded(
+                                child: Text(
+                                  _taskList.task[index].todoString,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16,
+                                    color: Colors.grey[800],
+                                  ),
+                                ),
+                              ),
+
+                              // Spacing
+                              SizedBox(
+                                width: 4,
+                              ),
+
+                              // Complete Icon
+                              _taskList.task[index].isDone
+                                  ? Icon(
+                                      Icons.check_circle,
+                                      color: Colors.white,
+                                    )
+                                  : Container(),
+                            ],
+                          ),
+
+                          // Spacing
+                          SizedBox(
+                            height: 18,
+                          ),
+
+                          // Task Detail
+                          Row(
+                            children: [
+                              // Tag String
+                              Text(
+                                _taskList.task[index].tagString,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 12,
+                                  color: _taskList.task[index].isDone ? Colors.white : Colors.grey[500],
+                                ),
+                              ),
+
+                              // Spacing
+                              Spacer(),
+
+                              // Due Date String
+                              _taskList.task[index].isDone
+                                  ? Text(
+                                      "COMPLETED",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 12,
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                  : Text(
+                                      _taskList.task[index].dueDateString,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 12,
+                                        color: Colors.grey[500],
+                                      ),
+                                    ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => Divider(
+                  height: 16,
+                  color: Colors.transparent,
+                ),
+                itemCount: _taskList.task != [] ? _taskList.task.length : 0,
+              ),
+            ),
+
+            // Spacing
+            Spacer(
+              flex: 1,
+            ),
+
             // Create Task
             Container(
               width: double.infinity,
@@ -58,15 +186,13 @@ class _ViewTasksPageState extends State<ViewTasksPage> {
                     color: Colors.white,
                   ),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) => CreateNewTaskPage(),
-                    ),
-                  );
-                },
+                onPressed: () {},
               ),
+            ),
+
+            // Fixed Spacing
+            SizedBox(
+              height: 48,
             ),
           ],
         ),
