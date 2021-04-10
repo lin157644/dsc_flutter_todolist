e1b8f1537a1c30f29312a8479ace9b0e65dee4df
FlutLab.io <>
2021-04-10 06:49:25 +0000

    Autosave

--- lib/views/viewTasksPage.dart
+++ lib/views/viewTasksPage.dart
@@ -61,113 +61,126 @@ class _ViewTasksPageState extends State<ViewTasksPage> {
               child: ListView.separated(
                 physics: BouncingScrollPhysics(),
                 itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
+                  return Dismissible(
+                    key: UniqueKey(),
+                    onDismissed: (direction) {
+                      // Remove the item from the data source.
                       setState(() {
-                        _taskList.task[index].isDone = !_taskList.task[index].isDone;
-                        _taskList.sortTask();
+                        _taskList.task.removeAt(index);
                       });
-                    },
-                    onLongPress: () {
-                      setState(() {
-                        _taskList.task[index].isPinned = !_taskList.task[index].isPinned;
 
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          textSnackBar(_taskList.task[index].isPinned ? "Task pinned." : "Task unpinned."),
-                        );
-
-                        _taskList.sortTask();
-                      });
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        textSnackBar("Task dismissed."),
+                      );
                     },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 18,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: _taskList.task[index].isPinned
-                            ? Border.all(
-                                color: Colors.blueGrey[900],
-                                width: 2.0,
-                              )
-                            : Border.all(color: Colors.blueGrey[100]),
-                        color: _taskList.task[index].isDone ? const Color(0xFFFFBC0A) : Colors.transparent,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // Todo String
-                              Expanded(
-                                child: Text(
-                                  _taskList.task[index].todoString,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 16,
-                                    color: Colors.grey[800],
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _taskList.task[index].isDone = !_taskList.task[index].isDone;
+                          _taskList.sortTask();
+                        });
+                      },
+                      onLongPress: () {
+                        setState(() {
+                          _taskList.task[index].isPinned = !_taskList.task[index].isPinned;
+
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            textSnackBar(_taskList.task[index].isPinned ? "Task pinned." : "Task unpinned."),
+                          );
+
+                          _taskList.sortTask();
+                        });
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: _taskList.task[index].isPinned
+                              ? Border.all(
+                                  color: Colors.blueGrey[900],
+                                  width: 2.0,
+                                )
+                              : Border.all(color: Colors.blueGrey[100]),
+                          color: _taskList.task[index].isDone ? const Color(0xFFFFBC0A) : Colors.transparent,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Todo String
+                                Expanded(
+                                  child: Text(
+                                    _taskList.task[index].todoString,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16,
+                                      color: Colors.grey[800],
+                                    ),
                                   ),
                                 ),
-                              ),
 
-                              // Spacing
-                              SizedBox(
-                                width: 4,
-                              ),
-
-                              // Complete Icon
-                              _taskList.task[index].isDone
-                                  ? Icon(
-                                      Icons.check_circle,
-                                      color: Colors.white,
-                                    )
-                                  : Container(),
-                            ],
-                          ),
-
-                          // Spacing
-                          SizedBox(
-                            height: 18,
-                          ),
-
-                          // Task Detail
-                          Row(
-                            children: [
-                              // Tag String
-                              Text(
-                                _taskList.task[index].tagString,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 12,
-                                  color: _taskList.task[index].isDone ? Colors.white : Colors.grey[500],
+                                // Spacing
+                                SizedBox(
+                                  width: 4,
                                 ),
-                              ),
 
-                              // Spacing
-                              Spacer(),
-
-                              // Due Date String
-                              _taskList.task[index].isDone
-                                  ? Text(
-                                      "COMPLETED",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w900,
-                                        fontSize: 12,
+                                // Complete Icon
+                                _taskList.task[index].isDone
+                                    ? Icon(
+                                        Icons.check_circle,
                                         color: Colors.white,
+                                      )
+                                    : Container(),
+                              ],
+                            ),
+
+                            // Spacing
+                            SizedBox(
+                              height: 18,
+                            ),
+
+                            // Task Detail
+                            Row(
+                              children: [
+                                // Tag String
+                                Text(
+                                  _taskList.task[index].tagString,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 12,
+                                    color: _taskList.task[index].isDone ? Colors.white : Colors.grey[500],
+                                  ),
+                                ),
+
+                                // Spacing
+                                Spacer(),
+
+                                // Due Date String
+                                _taskList.task[index].isDone
+                                    ? Text(
+                                        "COMPLETED",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 12,
+                                          color: Colors.white,
+                                        ),
+                                      )
+                                    : Text(
+                                        _taskList.task[index].dueDateString,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 12,
+                                          color: Colors.grey[500],
+                                        ),
                                       ),
-                                    )
-                                  : Text(
-                                      _taskList.task[index].dueDateString,
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 12,
-                                        color: Colors.grey[500],
-                                      ),
-                                    ),
-                            ],
-                          )
-                        ],
+                              ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   );
