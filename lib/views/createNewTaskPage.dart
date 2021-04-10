7b134cf94b8dcddc28d1c45e9eb67cc9c522c121
FlutLab.io <>
2021-04-10 05:38:26 +0000

    Autosave

--- lib/views/createNewTaskPage.dart
+++ lib/views/createNewTaskPage.dart
@@ -9,79 +9,82 @@ class CreateNewTaskPage extends StatefulWidget {
 
 //_privateClass _privateMethod _privateVariable
 class _CreateNewTaskPageState extends State<CreateNewTaskPage> {
-  @override
-  void initState() {
-    super.initState();
-  }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
+    final TextEditingController _contentController = TextEditingController();
+    
+    @override
+    void initState() {
+        super.initState();
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset: false, // Prevent Keyboard Overflow
-        body: Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Column(
-                children: [
-                    // Close (so fucking small like ad)
-                    Container(
-                        width: double.infinity,
-                        alignment: Alignment.centerRight,
-                        child: Transform.scale(
-                            scale: 0.8,
-                            child: IconButton(
-                            icon: Icon(Icons.close),
-                            onPressed: () {
-                                Navigator.pop(context);
-                            },
+    @override
+    void dispose() {
+        super.dispose();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            resizeToAvoidBottomInset: false, // Prevent Keyboard Overflow
+            body: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
+                    children: [
+                        // Close (so fucking small like ad)
+                        Container(
+                            width: double.infinity,
+                            alignment: Alignment.centerRight,
+                            child: Transform.scale(
+                                scale: 0.8,
+                                child: IconButton(
+                                icon: Icon(Icons.close),
+                                onPressed: () {
+                                    Navigator.pop(context);
+                                },
+                                ),
                             ),
                         ),
-                    ),
-                    // Title
-                    Container(
-                    alignment: Alignment.centerLeft,
-                        child: Text(
-                            "Create New Task",
+                        // Title
+                        Container(
+                        alignment: Alignment.centerLeft,
+                            child: Text(
+                                "Create New Task",
+                                style: TextStyle(
+                                    fontSize: 50,
+                                    height: 1.2,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.grey[800],
+                                    letterSpacing: 1.05,
+                                ),
+                            ),
+                        ),
+                        // Input Task TextField
+                        TextField(
                             style: TextStyle(
-                                fontSize: 50,
-                                height: 1.2,
-                                fontWeight: FontWeight.w700,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
                                 color: Colors.grey[800],
                                 letterSpacing: 1.05,
                             ),
-                        ),
-                    ),
-                    // Input Task TextField
-                    TextField(
-                        style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.grey[800],
-                            letterSpacing: 1.05,
-                        ),
-                        decoration: InputDecoration(
-                            enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blueGrey[100]),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blueGrey[300]),
-                            ),
-                            hintText: "Task Name",
-                            hintStyle: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.grey[400],
+                            decoration: InputDecoration(
+                                enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blueGrey[100]),
+                                ),
+                                focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blueGrey[300]),
+                                ),
+                                hintText: "Task Name",
+                                hintStyle: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.grey[400],
+                                ),
                             ),
                         ),
-                    ),
-                ],
+                    ],
+                ),
             ),
-        ),
-    );
-  }
+        );
+    }
 }
