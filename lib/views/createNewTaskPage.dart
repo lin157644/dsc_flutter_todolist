71a11747999d9a92c6c7ec11c1881c4f7ed089fd
FlutLab.io <>
2021-04-10 05:35:47 +0000

    Autosave

--- lib/views/createNewTaskPage.dart
+++ lib/views/createNewTaskPage.dart
@@ -22,65 +22,66 @@ class _CreateNewTaskPageState extends State<CreateNewTaskPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Column(
-            children: [
-                // Close (so fucking small like ad)
-                Container(
-                width: double.infinity,
-                alignment: Alignment.centerRight,
-                child: Transform.scale(
-                    scale: 0.8,
-                    child: IconButton(
-                    icon: Icon(Icons.close),
-                    onPressed: () {
-                        Navigator.pop(context);
-                    },
-                    ),
-                ),
-                ),
-                // Title
-                Container(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                    "Create New Task",
-                    style: TextStyle(
-                    fontSize: 50,
-                    height: 1.2,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.grey[800],
-                    letterSpacing: 1.05,
-                    ),
-                ),
-                ),
-                // Input Task TextField
-                TextField(
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey[800],
-                        letterSpacing: 1.05,
-                    ),
-                    decoration: InputDecoration(
-                        enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blueGrey[100]),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blueGrey[300]),
-                        ),
-                        hintText: "Task Name",
-                        hintStyle: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey[400],
+        resizeToAvoidBottomInset: false, // Prevent Keyboard Overflow
+        body: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+                children: [
+                    // Close (so fucking small like ad)
+                    Container(
+                        width: double.infinity,
+                        alignment: Alignment.centerRight,
+                        child: Transform.scale(
+                            scale: 0.8,
+                            child: IconButton(
+                            icon: Icon(Icons.close),
+                            onPressed: () {
+                                Navigator.pop(context);
+                            },
+                            ),
                         ),
                     ),
-                ),
-            ],
+                    // Title
+                    Container(
+                    alignment: Alignment.centerLeft,
+                        child: Text(
+                            "Create New Task",
+                            style: TextStyle(
+                                fontSize: 50,
+                                height: 1.2,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.grey[800],
+                                letterSpacing: 1.05,
+                            ),
+                        ),
+                    ),
+                    // Input Task TextField
+                    TextField(
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey[800],
+                            letterSpacing: 1.05,
+                        ),
+                        decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blueGrey[100]),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.blueGrey[300]),
+                            ),
+                            hintText: "Task Name",
+                            hintStyle: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey[400],
+                            ),
+                        ),
+                    ),
+                ],
+            ),
         ),
-      ),
     );
   }
 }
